a38e0d9672008a2887c31d4b794957ada48ac94e
amolverm <amol.verma@edifecs.com>
2016-04-01 02:04:00 +0530

    added testing.xml to framework. added new testcase for tracking

--- qa/src/main/java/PageObjects/Tracking.java
+++ qa/src/main/java/PageObjects/Tracking.java
@@ -4,6 +4,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,12 +12,50 @@ import java.util.List;
  */
 public class Tracking {
     List<WebElement> trackingElements;
-    WebElement tracking,userEmail;
+    List<WebElement> trackingRows = new ArrayList<WebElement>();
+    WebElement tracking,addButton,addNew,saveButton;
     public void setTrackingElements(AndroidDriver aDriver){
-        trackingElements=aDriver.findElements(By.id("tracking_recyclerView"));
+        trackingElements=aDriver.findElements(By.xpath("//android.support.v7.widget.RecyclerView[@resource-id='com.rgp.breathe:id/tracking_recyclerView']/android.widget.LinearLayout"));
+        //tracking= aDriver.findElement(By.xpath("//android.support.v7.widget.RecyclerView[@resource-id='com.rgp.breathe:id/tracking_recyclerView']/android.widget.LinearLayout"));
+        if(trackingElements.size()>0) {
+            for (int i = 1; i <= trackingElements.size(); i++) {
+                tracking= aDriver.findElement(By.xpath("//android.support.v7.widget.RecyclerView[@resource-id='com.rgp.breathe:id/tracking_recyclerView']/android.widget.LinearLayout["+i+"]/android.widget.LinearLayout[1]/android.widget.TextView[3]"));
+                trackingRows.add(tracking);
+            }
+        }
     }
 
-    public List<WebElement> getUserName(){
+    public List<WebElement> getTrackingElements(){
+        System.out.println(trackingRows.size());
         return trackingElements;
+    }
+
+    public List<WebElement> getTrackingRows(){
+        System.out.println(trackingRows.size());
+        return trackingRows;
+    }
+
+    public void setAddButton(AndroidDriver aDriver){
+        addButton=aDriver.findElement(By.id("com.rgp.breathe:id/fab"));
+    }
+
+    public void clickAddButton(){
+        addButton.click();
+    }
+
+    public void setAddNew(AndroidDriver aDriver){
+        addNew=aDriver.findElement(By.id("com.rgp.breathe:id/snackbar_action"));
+    }
+
+    public void clickAddNew(){
+        addNew.click();
+    }
+
+    public void setSaveButton(AndroidDriver aDriver){
+        saveButton=aDriver.findElement(By.id("com.rgp.breathe:id/action_save"));
+    }
+
+    public void clickSaveButton(){
+        saveButton.click();
     }
 }
---
+++ qa/src/test/java/TestTracking.java
@@ -0,0 +1,62 @@
+import Functions.AndroidSetup;
+import PageObjects.LeftMenu;
+import PageObjects.LoginPage;
+import PageObjects.Tracking;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by amolverm on 3/31/2016.
+ */
+public class TestTracking {
+    AndroidSetup androidSetup = new AndroidSetup();
+    LoginPage loginPage = new LoginPage();
+    LeftMenu leftmenu = new LeftMenu();
+    Tracking tracking = new Tracking();
+    List<WebElement> trackingsList,trackingsRowsList;
+    @BeforeClass
+    public void setElements() throws IOException {
+        androidSetup.andySetup();
+        loginPage.setLoginButton(androidSetup.aDriver);
+        loginPage.loginButtonClick();
+        leftmenu.getleftnav(androidSetup.aDriver);
+        leftmenu.leftnavclick();
+        androidSetup.aDriver.findElement(By.xpath("//android.widget.FrameLayout[@resource-id='com.rgp.breathe:id/nav_view']/android.support.v7.widget.RecyclerView//android.widget.CheckedTextView[contains(@text,'Tracking')]")).click();
+
+    }
+    @BeforeTest
+    public void addRows() throws InterruptedException {
+        tracking.setAddButton(androidSetup.aDriver);
+        tracking.clickAddButton();
+        tracking.setAddNew(androidSetup.aDriver);
+        tracking.clickAddNew();
+        tracking.setSaveButton(androidSetup.aDriver);
+        tracking.clickSaveButton();
+        Thread.sleep(2000);
+        tracking.clickAddButton();
+        tracking.clickAddNew();
+        tracking.clickSaveButton();
+        Thread.sleep(2000);
+        tracking.setTrackingElements(androidSetup.aDriver);
+    }
+    @Test
+    public void verifyRowCount() throws InterruptedException {
+        trackingsList = tracking.getTrackingElements();
+        trackingsRowsList = tracking.getTrackingRows();
+        assert (trackingsList.size() == 2);
+    }
+
+    @Test
+    public void verifyStat(){
+        System.out.println(trackingsList.get(0).findElement(By.className("android.widget.LinearLayout")).findElement(By.className("android.widget.TextView")).getText().toString());
+        //System.out.println(trackingsList.get(0).findElement(By.xpath(".//android.widget.LinearLayout")).getText().toString());
+        System.out.println(trackingsRowsList.get(0).getText().toString());
+        System.out.println(trackingsRowsList.get(1).getText().toString());
+    }
+}
