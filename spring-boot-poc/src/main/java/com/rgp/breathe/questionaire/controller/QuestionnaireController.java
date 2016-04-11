530234049efd3df0c399892ecf8f8b61c81828ec
munichan <munish.chandel@edifecs.com>
2016-04-11 11:08:28 +0530

    Added API to return dummy Risk Score based on questionnaire response

--- spring-boot-poc/src/main/java/com/rgp/breathe/questionaire/controller/QuestionnaireController.java
+++ spring-boot-poc/src/main/java/com/rgp/breathe/questionaire/controller/QuestionnaireController.java
@@ -1,22 +1,41 @@
 package com.rgp.breathe.questionaire.controller;
 
 import com.rgp.breathe.questionaire.dto.Questionnaire;
+import com.rgp.breathe.questionaire.dto.QuestionnaireResponse;
+import com.rgp.breathe.questionaire.dto.RiskScore;
 import com.rgp.breathe.questionaire.services.QuestionnaireService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("/questionnaire/")
 public class QuestionnaireController {
+    private final Logger logger = LoggerFactory.getLogger(QuestionnaireController.class);
 
     @Autowired
     private QuestionnaireService questionnaireService;
-    
+
     @RequestMapping("/{id}")
-    public Questionnaire index(@PathVariable("id") long id) {
-        return questionnaireService.getQuestionnaire(id);
+    public ResponseEntity<Questionnaire> getQuestionnaire(@PathVariable("id") long id, HttpServletRequest request) {
+        logger.info("Received request {} from IP {}", request.getRequestURI(), request.getRemoteHost());
+        return new ResponseEntity<>(questionnaireService.getQuestionnaire(id), HttpStatus.OK);
     }
-    
+
+    @RequestMapping(value = "/response", method = RequestMethod.POST)
+    public ResponseEntity<RiskScore> receiveResponse(@RequestBody QuestionnaireResponse questionnaireResponse) {
+        logger.info("Received response from questionnaireResponse = " + questionnaireResponse);
+        RiskScore dummyRiskScore = new RiskScore();
+        dummyRiskScore.setScore(ThreadLocalRandom.current().nextDouble(1, 10));
+
+        return new ResponseEntity<>(dummyRiskScore, HttpStatus.CREATED);
+    }
+
 }
---
+++ spring-boot-poc/src/main/java/com/rgp/breathe/questionaire/dto/RiskScore.java
@@ -0,0 +1,17 @@
+package com.rgp.breathe.questionaire.dto;
+
+/**
+ * Created by munichan on 11-04-2016.
+ */
+public class RiskScore {
+
+    private double score;
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+}
