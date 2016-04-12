2e61a31b7f7d700d103e54f478bfcb4626750386
Munish Chandel <munish.chandel@edifecs.com>
2016-04-12 09:36:34 +0530

    Added support for different style of locale passing

--- spring-boot-poc/src/main/java/com/rgp/breathe/questionaire/services/FileBasedQuestionnaireService.java
+++ spring-boot-poc/src/main/java/com/rgp/breathe/questionaire/services/FileBasedQuestionnaireService.java
@@ -8,15 +8,25 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by munichan on 11-04-2016.
  */
 @Service
 public class FileBasedQuestionnaireService {
+    private Set<String> availableLanguages = new HashSet<>();
+
+    public FileBasedQuestionnaireService() {
+        availableLanguages.add("en");
+        availableLanguages.add("en-us");
+        availableLanguages.add("hi");
+        availableLanguages.add("hi-in");
+    }
 
     public Questionnaire read(String language) throws IOException {
-        if (language == null || language.isEmpty()) {
+        if (language == null || language.isEmpty() || !availableLanguages.contains(language.toLowerCase())) {
             language = "en";
         }
         ObjectMapper mapper = new ObjectMapper();
