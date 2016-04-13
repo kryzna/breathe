a3337f4e8f884608db559e954208a04e1bd45ad0
amolverm <amol.verma@edifecs.com>
2016-04-13 14:12:15 +0530

    added and updated more test cases

--- qa/src/test/java/com/edifecs/breathe/qa/testcases/TestHealthAssessmentQuestions.java
+++ qa/src/test/java/com/edifecs/breathe/qa/testcases/TestHealthAssessmentQuestions.java
@@ -5,22 +5,32 @@ import com.edifecs.breathe.qa.functions.StandardFunctions;
 import com.edifecs.breathe.qa.pageobjects.HealthRiskAssesment;
 import com.edifecs.breathe.qa.functions.Andriodsetup;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Factory;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 /**
  * Created by amolverm on 4/11/2016.
  */
 public class TestHealthAssessmentQuestions {
-    HealthRiskAssesment healthRiskAssesment = new HealthRiskAssesment();
-    ExcelReader excelReader = new ExcelReader();
-    private String questionTitle;
+    //HealthRiskAssesment healthRiskAssesment = new HealthRiskAssesment();
+    public static ExcelReader excelReader = new ExcelReader();
+    private static String questionTitle;
+    private int expectedAnswerCount,actualAnswerCount;
+    private String expectedAnswerText,actualAnswerText;
 
-    @Test(priority = 1,invocationCount = 1)
+    @BeforeClass
+    public void setBeforeTest(){
+        if(excelReader.getCellData("RiskAssessment","Type",TestHealthRiskAssessment.questionNumber).toString().equals("r")) {
+            HealthRiskAssesment.setListAnswerOptions(Andriodsetup.aDriver);
+            TestHealthRiskAssessment.listAnswersOptions = HealthRiskAssesment.getListAnswerOptions();
+        } else if(excelReader.getCellData("RiskAssessment","Type",TestHealthRiskAssessment.questionNumber).toString().equals("c")) {
+            HealthRiskAssesment.setListCheckboxAnswerOptions(Andriodsetup.aDriver);
+            TestHealthRiskAssessment.listAnswersOptions = HealthRiskAssesment.getListCheckboxAnswerOptions();
+        }
+    }
+
+    @Test(priority = 1)
     // TC2 - If selected
-    public void verifyQuestionTitle(){
+    public static void verifyQuestionTitle(){
         try {
             questionTitle=excelReader.getCellData("RiskAssessment","Title",TestHealthRiskAssessment.questionNumber);
             System.out.println(questionTitle);
@@ -33,28 +43,55 @@ public class TestHealthAssessmentQuestions {
         }
     }
 
-    @Test(priority = 2,invocationCount = 1)
+    @Test(priority = 2)
     // TC1 - No option selected
     public void verifyAnswerOptionsCount(){
         try {
-            int answersCount = excelReader.getColumnCount("RiskAssessment", TestHealthRiskAssessment.questionNumber);
-            System.out.println(answersCount);
-            assert (TestHealthRiskAssessment.listAnswersOptions.size()==5);
+            expectedAnswerCount = excelReader.getColumnCount("RiskAssessment", TestHealthRiskAssessment.questionNumber)/2;
+            System.out.println(expectedAnswerCount);
+            actualAnswerCount = TestHealthRiskAssessment.listAnswersOptions.size();
+            assert (actualAnswerCount==expectedAnswerCount);
         }
         catch(Exception e)
         {
+            System.out.println("Expected: "+expectedAnswerCount+" and Actual: "+actualAnswerCount);
             e.printStackTrace();
         }
+    }
+
+    @Test(priority = 3)
+    public void verifyAnswersText(){
+        int i;
+        try{
+            for(i=1;i<=5;i++) {
+                expectedAnswerText=excelReader.getCellData("RiskAssessment", "choice"+i, TestHealthRiskAssessment.questionNumber);
+                actualAnswerText=TestHealthRiskAssessment.listAnswersOptions.get(i-1).getText().toString();
+                assert (TestHealthRiskAssessment.listAnswersOptions.get(i-1).getText().toString().equals(excelReader.getCellData("RiskAssessment", "choice"+i, TestHealthRiskAssessment.questionNumber)));
+            }
+        }   catch (Exception e){
+            System.out.println("Expected: "+expectedAnswerText+" and Actual: "+actualAnswerText);
+            e.printStackTrace();
+        }
+    }
+
+    @Test(priority = 4)
+    public void verifyRadioAnswerSelection(){
+        int radioSelectionNumber = 2;
+        TestHealthRiskAssessment.listAnswersOptions.get(radioSelectionNumber-1).click();
+        assert (TestHealthRiskAssessment.listAnswersOptions.get(radioSelectionNumber-1).getAttribute("checked").toString().equals("true"));
+    }
+    @AfterClass
+    public void moveToNextQuestion(){
         WebElement elementNextButton =   HealthRiskAssesment.getElementNextButton();
         StandardFunctions.click(elementNextButton);
         TestHealthRiskAssessment.questionNumber=TestHealthRiskAssessment.questionNumber+1;
     }
 
-    @Factory
+   /* @Factory
     public Object[] create(){
         return new Object[]{
                 new TestHealthAssessmentQuestions(), new TestHealthAssessmentQuestions(), new TestHealthAssessmentQuestions()
         };
-    }
+    }*/
 
 }
